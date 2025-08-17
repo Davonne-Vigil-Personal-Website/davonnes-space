3fd02130b981ba8bd1d0a128ba9d472398760b18
Davonne Vigil <davonnejv@gmail.com>
2025-08-16 21:30:49 -0500

    create: Header, AboutMe Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import "./App.css";
+import AboutMe from "./components/AboutMe";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <h1 className="font-mono text-3xl text-pink-500">Loading Awesomeness..!</h1>
+    <main className="bg-ourPink h-screen ">
+      <Header />
+      <AboutMe />
+    </main>
   );
 }
 
