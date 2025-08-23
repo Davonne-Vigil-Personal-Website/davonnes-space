ce73c680ba2c9d0bd0424408cf1b23db12b51210
Davonne Vigil <davonnejv@gmail.com>
2025-08-23 13:13:03 -0500

    map over my tech stack, start on styles

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -10,7 +10,16 @@ export default function Technologies() {
   ];
   return (
     <section className="mt-8">
-      <h1 className="font-grav text-3xl">Tech Stack</h1>
+      <h1 className="font-grav text-2xl bg-black text-white p-2 w-60">
+        Tech Stack
+      </h1>
+      <div className="flex flex-col gap-4 mt-4">
+        {myTechStack.map((techStack, index) => (
+          <button key={index} className="border border-black p-2 w-60 ">
+            {techStack.tech}
+          </button>
+        ))}
+      </div>
     </section>
   );
 }
